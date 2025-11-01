c3c6abeb5454b9c86567a5155df134f337c4278b
drh8820 <d131619@163.com>
2025-11-01 16:35:11 -0500

    upload

---
+++ app/src/main/java/com/example/chatroom/Resource.java
@@ -0,0 +1,10 @@
+package com.example.chatroom;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Resource {
+    public static List<String> onLineAccounts= new ArrayList<String>();
+    public static String JsonUrl = "http://localhost:8080//ChatRoomJsonServer/JsonServlet";
+    public static String DiffUrl = "http://10.189.124.122:8080//ChatRoom/DiffServlet";
+}
